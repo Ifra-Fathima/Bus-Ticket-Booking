4705a25623a17f63c018dfcf5080ebe69863bf51
ifra fathima <ifrak264@gmail.com>
2025-01-07 17:47:41 +0530

    migrating database to mongodb

--- middleware/adminMiddleware.js
+++
@@ -1,8 +0,0 @@
-const adminMiddleware=(req,res,next)=>{
-    if(!req.user.is_admin){
-        return res.status(403).json({message:'Access denied. Admins only!'});
-    }
-    next();
-};
-
-export default adminMiddleware;
